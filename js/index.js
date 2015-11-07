b82c49cd439acb8e7e6807ebcb6c1a3dd882ef8d
Braden Jonsson <braden337@gmail.com>
2015-11-06 23:03:43 -0600

    clean up and add comments

--- js/index.js
+++ js/index.js
@@ -1,70 +1,81 @@
-/* Variables */
+// Author: @braden337
 
-var types = ["#width", "#height", "#diameter"];
+
+
+  /* --------- */
+ /* Variables */
+/* --------- */
+
+var idName = ["#width", "#height", "#diameter"];
+var originalField = ["#width", "#height", "#diameter", "#width2", "#height2", "#diameter2"];
 var field = ["#width", "#height", "#diameter", "#width2", "#height2", "#diameter2"];
 var position = 0;
 var numCompared = 2;
 
-/* Events */
 
+  /* ------ */
+ /* Events */
+/* ------ */
+
+/* on keyup in fields, do stuff */
 $("form").on("keyup", "input[type=tel]", function(){
   var length = $(this).val().length;
   var max = $(this).attr("maxlength");
   var lastField = field.length-1;
-  //console.log("lastField " + lastField + " position " + position);
   
   if (length == max && position < lastField) {
     position += 1;
-    $(field[position]).focus()//.trigger('touchstart');
+    $(field[position]).focus(); //.trigger('touchstart');
   }
+
   if (length == max && position == lastField) {
-    $(this).blur();
-    // $("button#reset").focus();
+    $(this).blur(); // $("button#reset").focus();
   }
 
   var lastDiameter = $("#diameter" + numCompared);
+
   if (lastDiameter.val() != "" && lastDiameter.val().length == lastDiameter.attr("maxlength")) {
-    enableAdd()
+    enableAdd();
   }
 
   if (lastDiameter.val() == "") {
-    disableAdd()
+    disableAdd();
   } 
 });
 
+
+/* on touch of fields, focus */
 $('form').on('touchstart', "input[type=tel]", function () {
-    $(this).focus();
-    // $(field[position]).focus();
+    $(this).focus(); // $(field[position]).focus();
 });
 
-
-// This clears the input on focus and sets the position variable to the index of the current field that's focused
+/* on focus of fields, clear the field and
+   set the the position to the index of  */
 $("form").on("focus", "input[type=tel]", function(){
   $(this).val('');
   var currentField = field.indexOf("#" + $(this).attr('id'));
   position = currentField;
 });
 
-//clears the input when you click
-// $("form").on("click", "input[type=tel]", function(){
-//   $(this).val('');
-// });
-
+/* on click of reset button, remove all fields
+   except 2, and other stuff */
 $("#controls").on("click", "#reset", function(){
   $('form').empty();
   addFields(generateFields(""));
   addFields(generateFields(2));
-  field = ["#width", "#height", "#diameter", "#width2", "#height2", "#diameter2"];
+  field = originalField;//["#width", "#height", "#diameter", "#width2", "#height2", "#diameter2"];
   numCompared = 2;
   $(field[0]).focus();
   disableAdd();
 });
 
+/* on click of add tire button, insert a set of
+   3 fields in the end of the form tag */
 $("#controls").on("click", "#addTire", function(){
   numCompared+=1;
 
   for(i=0; i< 3; i++) {
-    field.push(types[i] + numCompared);
+    field.push(idName[i] + numCompared);
   }
 
   //console.log(field.length);
@@ -77,10 +88,10 @@ $("#controls").on("click", "#addTire", function(){
   }
 });
 
-// $("form").submit(function(event){
-//   event.preventDefault();
-// });
 
+  /* --------- */
+ /* Functions */
+/* --------- */
 
 function generateFields(num) {
   return "<div class=\"form-group col-lg-2 col-lg-offset-3 col-xs-4\"><label for=\"width" + num + "\">Width:</label><input type=\"tel\" class=\"form-control\" id=\"width" + num + "\" maxlength=\"3\" /></div><div class=\"form-group col-lg-2 col-xs-4\"><label for=\"height" + num + "\">Ratio:</label><input type=\"tel\" class=\"form-control\" id=\"height" + num + "\" maxlength=\"2\" /></div><div class=\"form-group col-lg-2 col-xs-4\"><label for=\"diameter" + num + "\">Diameter:</label><input type=\"tel\" class=\"form-control\" id=\"diameter" + num + "\" maxlength=\"2\" /></div>";
@@ -97,17 +108,3 @@ function disableAdd() {
 function enableAdd() {
   $("#addTire").attr('disabled', false);
 }
-
-// $(".form-group:nth-last-of-type(2)")
-
-
-
-
-
-
-
-
-
-
-
-
